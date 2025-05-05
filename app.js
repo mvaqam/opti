b3cfe2eba7b6262373b63b3fdbee236ec4d0e2d9
Manuel Angel Vaquerizo <55359596+mvaqam@users.noreply.github.com>
2025-05-05 18:07:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, '/public/index.html'));
 });
