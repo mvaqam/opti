9135b6a65d96b4c1b43c9096e8bb9e599b501027
Manuel Angel Vaquerizo <55359596+mvaqam@users.noreply.github.com>
2025-05-07 18:46:50 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+  res.sendFile(path.join(__dirname, '/public/login.html'));
+  });
 
 app.post('/guardar', (req, res) => {
     const { mail, pw } = req.body;
@@ -20,17 +22,38 @@ app.post('/guardar', (req, res) => {
   
     const filepath = 'myjsonfile.json';
     let data = { table: [] };
-  
     if (fs.existsSync(filepath)) {
       const raw = fs.readFileSync(filepath, 'utf8');
       data = JSON.parse(raw);
+    } else {
+      data.table.push({ mail, pw });
+      fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf8');
+      res.json({ message: "Datos guardados con éxito" });
     }
+
+  });
+
+  app.post('/login', (req, res) => {
+    const { mail, pw } = req.body;
+    const filepath = 'myjsonfile.json';
   
-    data.table.push({ mail, pw });
+    if (fs.existsSync(filepath)) {
+      const raw = fs.readFileSync(filepath, 'utf8');
+      const data = JSON.parse(raw);
   
-    fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf8');
+      // Buscar si hay un usuario con ese mail y contraseña
+      const user = data.table.find(user => user.mail === mail && user.pw === pw);
   
-    res.json({ message: "Datos guardados con éxito" });
+      if (user) {
+        // Usuario autenticado correctamente
+        res.json({ success: true });
+      } else {
+        // Usuario o contraseña incorrecta
+        res.status(401).json({ message: 'Correo o contraseña incorrectos' });
+      }
+    } else {
+      res.status(500).json({ message: 'No hay base de datos de usuarios' });
+    }
   });
 
 app.listen(PORT, () => {
